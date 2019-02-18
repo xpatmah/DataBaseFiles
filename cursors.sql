b9553d253868ae77ff420e693b7a1670acd0dc13
Mahesh Pathak <38168005+xpatmah@users.noreply.github.com>
2019-02-18 12:15:32 +0530

    Update cursors.sql

--- cursors.sql
+++ cursors.sql
@@ -6,3 +6,28 @@ open localCursor;
 fetch localCursor into l_depatment_id;
 dbms_output.put_line(l_depatment_id);
 end;
+
+declare 
+cursor localCursor is select employee_id from Employees where EMPLOYEE_ID=213; -- only for one arguments
+cursoronlocalCursor localCursor%rowtype;
+l_depatment_id number;
+begin
+open localCursor;
+fetch localCursor into cursoronlocalCursor;
+dbms_output.put_line(cursoronlocalCursor.employee_id);
+end;
+
+declare 
+cursor localCursor is select employee_id from Employees;
+cursoronlocalCursor localCursor%rowtype;
+l_depatment_id number;
+begin
+open localCursor;
+loop
+  fetch localCursor into cursoronlocalCursor;
+  Exit WHEN localCursor%NOTFOUND;
+  dbms_output.put_line(cursoronlocalCursor.employee_id);
+end loop;
+close localCursor;
+end;
+
